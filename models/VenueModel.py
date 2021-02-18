00aadc1686cd1fba1405e1be182773e4cbd84b27
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 08:25:23 +0200

    set max Venue.name size

--- models/VenueModel.py
+++ models/VenueModel.py
@@ -5,13 +5,13 @@ class Venue(db.Model):
     __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    facebook_link = db.Column(db.String(500))
     shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
