2da74c8ccd869699979c8abc138e825bef20cb3f
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 18:32:08 +0200

    implement search_artists controller

--- app.py
+++ app.py
@@ -176,16 +176,16 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-    # search for "band" should return "The Wild Sax Band".
+    search_term = request.form.get("search_term")
+    data = Artist.query.filter(Venue.name.ilike(f'%{search_term}%')).all()
     response = {
-        "count": 1,
+        "count": len(data),
         "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
+            "id": artist.id,
+            "name": artist.name,
+            "num_upcoming_shows": Show.query.with_parent(artist).filter(
+                Show.start_time > datetime.now()).count(),
+        } for artist in data]
     }
     return render_template('pages/search_artists.html', results=response,
                            search_term=request.form.get('search_term', ''))
