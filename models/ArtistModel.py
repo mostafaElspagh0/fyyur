64c8ab9e2a32b2e592c8dfe484e053474cfcb5a1
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 05:31:59 +0200

    rename table names to be in lower case plural form

--- models/ArtistModel.py
+++ models/ArtistModel.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
--- models/VenueModel.py
+++ models/VenueModel.py
@@ -2,7 +2,7 @@ from app import db
 
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
