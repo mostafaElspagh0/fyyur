36eba8f532035332c766c3b78087b5f5db5719fc
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 05:30:40 +0200

    add relationship to Show model

--- models/VenueModel.py
+++ models/VenueModel.py
@@ -12,5 +12,6 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='venue', lazy=True)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
