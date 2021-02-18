8d628dbc71b50df30058ce8fd0e3b40ed2c450fe
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 08:25:06 +0200

    set max Artist.name size

--- models/ArtistModel.py
+++ models/ArtistModel.py
@@ -5,7 +5,7 @@ class Artist(db.Model):
     __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
