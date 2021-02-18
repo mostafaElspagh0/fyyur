093b537b932dc87ecd26b447cb61c907242e6b8f
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 09:36:06 +0200

    implement many to many relation between genre and artist

--- models/ArtistModel.py
+++ models/ArtistModel.py
@@ -1,6 +1,7 @@
 from app import db
 from models.CityModel import City
 from models.StateModel import State
+from models.SeconderyTables import artist_genre_table
 
 
 class Artist(db.Model):
@@ -9,13 +10,11 @@ class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(500))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
-    genres = db.Column(db.String(500))
 
     # foreign keys
     city_id = db.Column(db.Integer, db.ForeignKey(f'{City.__tablename__}.id'))
@@ -23,3 +22,5 @@ class Artist(db.Model):
 
     # relations
     shows = db.relationship('Show', backref='artist', lazy=True)
+    genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref('artists'))
+
--- models/SeconderyTables.py
+++ models/SeconderyTables.py
@@ -4,3 +4,8 @@ venue_genreTable = db.Table('venue_genre_table',
                             db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
                             db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
                             )
+
+artist_genre_table = db.Table('artist_genre_table',
+                              db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+                              db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+                              )
