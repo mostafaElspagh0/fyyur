b325d02126d6281951d5766927129aa9709d12a3
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 05:27:28 +0200

    create show model

---
+++ models/ShowModel.py
@@ -0,0 +1,16 @@
+from app import db
+from .ArtistModel import Artist
+from .VenueModel import Venue
+
+
+class Show(db.Model):
+    __tablename__ = "shows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey(f'{Artist.__tablename__}.id'))
+    venue_id = db.Column(db.Integer, db.ForeignKey(f'{Venue.__tablename__}.id'))
+    start_time = db.Column(db.DateTime)
+    duration_inMinutes = db.Column(db.Integer , nullable=False ,default=100)
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.start_time}, {self.duration_inMinutes}>'
