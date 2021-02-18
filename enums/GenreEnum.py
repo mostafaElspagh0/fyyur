0c447b0b898af8dc68ab5ec929a731f500c60d53
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-18 15:13:06 +0200

    create Genre Enum

---
+++ enums/GenreEnum.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class Genre(Enum):
+    Alternative = 'Alternative'
+    Blues = 'Blues'
+    Classical = 'Classical'
+    Country = 'Country'
+    Electronic = 'Electronic'
+    Folk = 'Folk'
+    Funk = 'Funk'
+    Hip_Hop = 'Hip-Hop'
+    Heavy_Metal = 'Heavy Metal'
+    Instrumental = 'Instrumental'
+    Jazz = 'Jazz'
+    Musical_Theatre = 'Musical Theatre'
+    Pop = 'Pop'
+    Punk = 'Punk'
+    R_AND_B = 'R&B'
+    Reggae = 'Reggae'
+    Rock_n_Roll = 'Rock n Roll'
+    Soul = 'Soul'
+    Other = 'Other'
