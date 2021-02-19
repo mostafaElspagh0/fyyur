59eeee981b8d348baeb3198da0d30f92a70e1f3b
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-19 11:21:51 +0200

    upgrade "flask_wtf.Form" to "FlaskForm"

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, AnyOf, URL
 from enums import State, Genre
@@ -17,7 +17,7 @@ def genre_validate():
     return _validate
 
 
-class ShowForm(Form):
+class ShowForm(FlaskForm):
     artist_id = StringField(
         'artist_id'
     )
@@ -31,7 +31,7 @@ class ShowForm(Form):
     )
 
 
-class VenueForm(Form):
+class VenueForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -70,7 +70,7 @@ class VenueForm(Form):
     seeking_description = TextAreaField('seeking_description')
 
 
-class ArtistForm(Form):
+class ArtistForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
