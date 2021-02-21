0a244a39e32df8172e8e19bf4c64c5fa10bd0c3a
mostafa elspagh <mostafaelspagh0@gmail.com>
2021-02-21 22:16:20 +0200

    move shows controller to a separate file

--- app.py
+++ app.py
@@ -67,58 +67,6 @@ def index():
 from controllers import *
 
 
-#  Shows
-#  ----------------------------------------------------------------
-
-@app.route('/shows')
-def shows():
-    # displays list of shows at /shows
-    shows_data = Show.query.all()
-    data = []
-    for show in shows_data:
-        artist = show.artist
-        venue = show.venue
-        data.append({
-            "venue_id": venue.id,
-            "venue_name": venue.name,
-            "artist_id": artist.id,
-            "artist_name": artist.name,
-            "artist_image_link": artist.image_link,
-            "start_time": str(show.start_time)
-        })
-    return render_template('pages/shows.html', shows=data)
-
-
-@app.route('/shows/create')
-def create_shows():
-    # renders form. do not touch.
-    form = ShowForm()
-    return render_template('forms/new_show.html', form=form)
-
-
-@app.route('/shows/create', methods=['POST'])
-def create_show_submission():
-    error = False
-    try:
-        artist_id = request.form['artist_id']
-        venue_id = request.form['venue_id']
-        start_time = request.form['start_time']
-        show = Show(artist_id=artist_id, venue_id=venue_id, start_time=start_time)
-        db.session.add(show)
-        db.session.commit()
-    except Exception:
-        error = True
-        db.session.rollback()
-        print(sys.exc_info())
-    finally:
-        db.session.close()
-    if error:
-        flash('An error occurred. Show could not be listed.')
-    if not error:
-        flash('Show was successfully listed')
-    return render_template('pages/home.html')
-
-
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
